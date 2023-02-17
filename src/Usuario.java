fd4eb6fd21d5df176b9bbaaca3fca7f129814e0a
raphaelkauan <raphaelkauanoficial@gmail.com>
2023-02-17 16:02:55 -0400

    Atualizando o readme

--- src/Usuario.java
+++ src/Usuario.java
@@ -6,21 +6,20 @@ public class Usuario {
         System.out.println("Canal Atual: " + smartTv.canal);
         System.out.println("Volume Atual: " + smartTv.volume);
 
-
-         /* Chamando novos Métodos */
-        smartTv.ligar ();
+        /* Chamando novos Métodos */
+        smartTv.ligar();
         System.out.println("Novo Status -> TV ligada ? " + smartTv.ligada);
 
-        smartTv.diminuirVolume ();
-        smartTv.diminuirVolume ();
-        smartTv.diminuirVolume ();
-        smartTv.aumentarVolume ();
-        smartTv.diminuirVolume ();
-        
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+
         System.out.println("Novo Status -> Volume Atual: " + smartTv.volume);
 
         /* Mudar de Canal */
-        
+
         smartTv.mudarCanal(39); /* <- Insira o novo canal Aqui */
         System.out.println("Novo Status -> Canal Atual: " + smartTv.canal);
     }
